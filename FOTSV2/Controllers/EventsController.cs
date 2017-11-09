b7a50c3d41e3c298536e5392b39bbf8237af5da7
LoriFair <lorijfair@hotmail.com>
2017-11-09 17:07:55 -0500

    about page content and css

---
+++ FOTSV2/Controllers/EventsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FOTSV2.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Event
+        public ActionResult UpcomingEvents()
+        {
+            return View();
+        }
+
+        public ActionResult PetFest()
+        {
+            return View();
+        }
+    }
+
+
+}
---
+++ FOTSV2/Controllers/GetInvolvedController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FOTSV2.Controllers
+{
+    public class GetInvolvedController : Controller
+    {
+        // GET: GetInvolved
+        public ActionResult Membership()
+        {
+            return View();
+        }
+
+        public ActionResult Meetings()
+        {
+            return View();
+        }
+
+        public ActionResult Volunteer()
+        {
+            return View();
+        }
+    }
+}
